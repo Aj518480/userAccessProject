3e25500f7b000abb5eda1cff0827971a877d1de6
Allison Juarez <53452871+Aj518480@users.noreply.github.com>
2023-06-26 17:46:57 -0400

    sudo code in js to work out logic in head to help sort out what functions I will be using or what conditional will be needed for these game to work correctly

---
+++ movingUserAccess.js
@@ -0,0 +1,24 @@
+
+//sudo coding//
+//Once the page had loaded for the user
+// I want the main button start with the word "start" then have an onclick listener on so it can listen to change to word "enter" to enter the access code.
+//Once the onclick listener has go off I want the timer to start counting down from 30 
+//this will require a function to make a countdown clock
+//use jquery to put it on the page
+
+//this will need a function to make an array of 9 randomize numbers 
+//and the 9 randomize number to appear on the page.
+//(post on the page using jquery)
+//put these array of randomize number in a global var so it can be compared for later use.
+//then grab this array into a different function and it can pick random from the array
+// putting this number in a global variable so the conditional can see if it is the match number.
+//I need to set a few conditionals
+//1. to see if the enter code matches one of the randomize codes 
+//-stop clock
+//put title to change to "you have stopped the bomb you are everyones hero"
+//(celebrating gif) for 5 seconds restart game to start button
+//2. if the clock got to zero
+//-stop clock
+//-change title to "you lost everyone"
+//change bomb img to an exploding one
+//5 seconds change button to start again
